7888463c0b7b6bcf8d929a9a189c50c18beca124
Chris Burgess <chris@giantrobot.co.nz>
2017-03-26 09:33:41 +1300

    Move hooks to .hooks.php so we can generate automatically.

--- civideveloper.debug.php
+++ civideveloper.debug.php
@@ -2,6 +2,18 @@
 
 /**
  * Debug a single value and its name, the best available way.
+ *
+ * Report which hooks got called.
+ *
+ * Show detail on a specific hook. Showing detail on all hooks will probably be
+ * excessive - not hard to consume a lot of memory with dpm() and large objects.
+ *
+ * @TODO: WordPress debugging
+ *   - https://wordpress.org/plugins/query-monitor/ ?
+ *
+ * @TODO: Joomla debugging
+ *   - https://docs.joomla.org/How_to_debug_your_code ?
+ *   - https://github.com/mathiasverraes/jdump
  */
 function _civideveloper_debug($param, $name) {
   if (function_exists('dpm')) {
@@ -19,7 +31,19 @@ function _civideveloper_debug($param, $name) {
  * Debug a series of function arguments and the called hook.
  */
 function _civideveloper_debug_func_args($function, $args) {
+  return;
   foreach ($args as $name => $arg) {
     _civideveloper_debug($arg, $function . ': $' . $name);
   }
+}
+
+/**
+ * Extract a list of CiviCRM hooks.
+ *
+ * @TODO Include extension-provided hooks in results.
+ */
+function _civideveloper_list_hooks() {
+  $hooks = get_class_methods("CRM_Utils_Hook");
+  sort($hooks);
+  return $hooks;
 }
